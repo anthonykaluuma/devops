5d8460a95a074fe426cac2286859e08b238c8b92
Anthony Kalu Uma <anthonykaluuma@gmail.com>
2019-11-19 23:33:24 +0000

    add mongodb reponse state to client

--- client/src/App.js
+++ client/src/App.js
@@ -5,9 +5,13 @@ import "./App.css";
 class App extends Component {
     constructor(props) {
         super(props);
-        this.state = { apiResponse: "" };
+        this.state = {
+            apiResponse: "",
+            dbResponse: ""
+        };
     }
 
+    // Go to API and check testAPI route for a response
     callAPI() {
         fetch("http://localhost:9000/testAPI")
             .then(res => res.text())
@@ -15,20 +19,31 @@ class App extends Component {
             .catch(err => err);
     }
 
+    // Go to API and check testDB route for a response
+    callDB() {
+        fetch("http://localhost:9000/testDB")
+            .then(res => res.text())
+            .then(res => this.setState({ dbResponse: res }))
+            .catch(err => err);
+    }
+
+    // Execute the calls when componnent mounts
     componentDidMount() {
         this.callAPI();
+        this.callDB();
     }
 
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-                <p className="App-intro">{this.state.apiResponse}</p>
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">{this.state.apiResponse}</p>
+            <p className="App-intro">{this.state.dbResponse}</p>
             </div>
-        );
+    );
     }
 }
 
