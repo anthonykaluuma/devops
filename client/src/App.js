cc349b48991290e8906e703384ed9f0b1c7539c4
Anthony Kalu Uma <akaluuma@spotify.com>
2019-11-19 20:51:06 +0000

    clean up

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor(props) {
+      super(props);
+      this.state = { apiResponse: ""};
+    }
+
+  constructor(props) {
+    super(props);
+    this.state = { apiResponse: "" };
+  }
+  callAPI() {
+      fetch("http://localhost:9000/testAPI")
+          .then(res => res.text())
+          .then(res => this.setState({ apiResponse: res }));
+  }
+  componentWillMount() {
+      this.callAPI();
+  }
+
+  render(){
+    return (
+      <div className="BuildItApp">
+        <header className="BuildItApp-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="BuildItApp-title">Welcome to Anthonys Application</h1>
+        </header>
+        <p className="BuildItApp-intro">{this.state.apiResponse}</p>
+      </div>
+    );  
+  }    
 }
 
-export default App;
+export default App
