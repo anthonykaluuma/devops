6b650792d7b246f8d3d2b6568eb7b8eab5554543
Anthony Kalu Uma <akaluuma@spotify.com>
2019-11-19 21:30:17 +0000

    working setup!
    React frontend connected to node api

--- client/src/App.js
+++ client/src/App.js
@@ -4,34 +4,32 @@ import "./App.css";
 
 class App extends Component {
     constructor(props) {
-      super(props);
-      this.state = { apiResponse: ""};
+        super(props);
+        this.state = { apiResponse: "" };
     }
 
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-  callAPI() {
-      fetch("http://localhost:9000/testAPI")
-          .then(res => res.text())
-          .then(res => this.setState({ apiResponse: res }));
-  }
-  componentWillMount() {
-      this.callAPI();
-  }
+    callAPI() {
+        fetch("http://localhost:9000/testAPI")
+            .then(res => res.text())
+            .then(res => this.setState({ apiResponse: res }))
+            .catch(err => err);
+    }
 
-  render(){
-    return (
-      <div className="BuildItApp">
-        <header className="BuildItApp-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="BuildItApp-title">Welcome to Anthonys Application</h1>
-        </header>
-        <p className="BuildItApp-intro">{this.state.apiResponse}</p>
-      </div>
-    );  
-  }    
+    componentDidMount() {
+        this.callAPI();
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to React</h1>
+                </header>
+                <p className="App-intro">{this.state.apiResponse}</p>
+            </div>
+        );
+    }
 }
 
-export default App
+export default App;
